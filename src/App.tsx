5f3d790d82065ef4a797d532735d0e0052ac2404
City <city@nanzc.com>
2021-01-02 16:45:27 +0800

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import './index.css'
+
+interface IProps {
+  name: string
+  age: number
+}
+
+const obj: IProps[] = [{ name: 'wxd', age: 18 }]
 
 export default defineComponent({
   name: 'App',
   setup() {
+    const List = <div>LIST: </div>
+
     return () => (
       <>
-        <div id='nav'>
+        <div
+          id='nav'
+          onClick={() => {
+            console.log(111)
+          }}
+        >
           <RouterLink to='/'>Home</RouterLink>
         </div>
         <RouterView />
+
+        {List}
+
+        {obj.map(item => (
+          <div>
+            <div>{item.name}</div>
+            <div>{item.age}</div>
+          </div>
+        ))}
       </>
     )
   }
